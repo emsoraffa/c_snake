855e371b130ea9484180a6f0e307fbaeb84a4f3f
emsoraffa <emil.rafael.solberg@gmail.com>
2024-05-10 23:19:04 +0200

    locks

--- concurrency.c
+++ concurrency.c
@@ -1,22 +1,35 @@
 #include <pthread.h>
+#include <stdint.h>
 #include <stdio.h>
 
-pthread_cond_t c = PTHREAD_COND_INITIALIZER;
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t big_p = PTHREAD_MUTEX_INITIALIZER;
 
 void *someFunc(void *arg);
+void *diffFunc(void *arg);
+int count = 0;
 
 int main() {
-  pthread_t t1, t2;
+  pthread_t thread_a, thread_b;
 
-  pthread_create(&t1, NULL, someFunc, "Jåss");
-  pthread_create(&t2, NULL, someFunc, "kukkene bruse");
-  pthread_join(t1, NULL);
-  pthread_join(t2, NULL);
+  pthread_create(&thread_a, NULL, diffFunc, "");
+  pthread_create(&thread_b, NULL, diffFunc, "");
+  pthread_join(thread_a, NULL);
+  pthread_join(thread_b, NULL);
+
+  printf("%i\n", count);
   return 0;
 }
 
 void *someFunc(void *arg) {
   printf("%s\n", (char *)arg);
+  return NULL;
+}
+
+void *diffFunc(void *arg) {
+  for (uint64_t i = 0; i < 1000000; i++) {
+    pthread_mutex_lock(&big_p);
+    count += 1;
+    pthread_mutex_unlock(&big_p);
+  }
   return NULL;
 }
